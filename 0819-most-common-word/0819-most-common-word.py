6366efa29b8b263ab9cde6b1bc3fba3a9419097f
Nitik Mishra <nitikmishra29@gmail.com>
2026-07-09 22:56:24 +0530

    Time: 3 ms (62.71%), Space: 19.4 MB (51.7%) - LeetHub

--- 0819-most-common-word/0819-most-common-word.py
+++ 0819-most-common-word/0819-most-common-word.py
@@ -3,6 +3,6 @@ import re
 
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
-        words = re.sub(r"[^\w\s]", " ", paragraph.lower()).split()
+        words = re.findall(r'\w+', paragraph.lower())
         count = Counter(word for word in words if word not in banned)
         return max(count,key=count.get)
