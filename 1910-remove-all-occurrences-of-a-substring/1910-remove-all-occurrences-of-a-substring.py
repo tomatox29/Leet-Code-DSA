84eb4842d04a937c8b6f5f392bcfc610dc406f8b
Nitik Mishra <nitikmishra29@gmail.com>
2026-07-12 15:29:39 +0530

    Time: 0 ms (100%), Space: 19.2 MB (90.73%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.py
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.py
@@ -1,7 +1,14 @@
 class Solution:
     def removeOccurrences(self, s: str, part: str) -> str:
+        while part in s:
+            s = s.replace(part, "", 1)
+        return s 
+
+
+
         stack=[]
         part_len=len(part)
+
 
         for ch in s:
             stack.append(ch)
@@ -14,12 +21,7 @@ class Solution:
 
 
 
-
-
-        while part in s:
-            s = s.replace(part, "", 1)
-        return s 
-
+        
   
 
 
