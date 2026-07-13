6dfee63e76a230aa8378b1526fbcb827444956ea
Nitik Mishra <nitikmishra29@gmail.com>
2026-07-13 14:46:53 +0530

    Time: 46 ms (58.16%), Space: 19.2 MB (30.89%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,15 +1,8 @@
 class Solution:
     def reverse(self, x: int) -> int:
-
         sign = -1 if x < 0 else 1
-
         x = abs(x)
-
-        reversed_num = int(str(x)[::-1])
-
-        reversed_num *= sign
-
+        reversed_num = int(str(x)[::-1])* sign
         if reversed_num < -2**31 or reversed_num > 2**31 - 1:
             return 0
-
         return reversed_num
