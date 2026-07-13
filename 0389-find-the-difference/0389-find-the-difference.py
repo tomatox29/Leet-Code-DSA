c2d7b32af5ba40519a41df594f780c5a9fe94c9d
Nitik Mishra <nitikmishra29@gmail.com>
2026-07-13 19:32:22 +0530

    Time: 0 ms (100%), Space: 19.3 MB (23%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,13 +1,21 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
+        sum_t = sum(ord(ch) for ch in t)
+        sum_s = sum(ord(ch) for ch in s)
+
+        return chr(sum_t-sum_s)
+
+
+
+
+
         freq = {}
         for ch in s:
             freq[ch]=freq.get(ch,0)+1
         for ch in t:
             freq[ch]=freq.get(ch,0)-1
-            
-
-        # Find the extra character
         for key, value in freq.items():
             if value == -1:
                 return key 
+
+
