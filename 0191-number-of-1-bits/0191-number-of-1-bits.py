ee049c4daa8c1323ceea5b5b8e434cd40beb3d4f
Nitik Mishra <nitikmishra29@gmail.com>
2026-07-14 09:35:31 +0530

    Time: 0 ms (100%), Space: 19.3 MB (54.46%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -6,3 +6,15 @@ class Solution:
                 count+=1
             n>>=1
         return count 
+
+
+        count = 0
+        if n&n-1==0:
+            return 1
+
+        rest = bin(n)
+        for bit in rest:
+            if bit == 1:
+                count+=1
+
+        return count
