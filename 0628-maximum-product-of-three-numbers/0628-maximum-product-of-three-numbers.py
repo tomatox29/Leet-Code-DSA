9781c6b9da095581da8d2f20f6c1505df27fe97f
Nitik Mishra <nitikmishra29@gmail.com>
2026-07-26 10:51:47 +0530

    Time: 17 ms (70.48%), Space: 20.2 MB (97.21%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.py
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.py
@@ -1,5 +1,19 @@
 class Solution:
     def maximumProduct(self, nums: List[int]) -> int:
+        # max1,max2,max3=float(-inf)
+
+
+
+
+
+
+
+
+
+
+
+
+
         nums.sort()
         return max(
             nums[-1] * nums[-2] * nums[-3],
