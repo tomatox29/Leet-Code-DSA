674c864a2ca7e3831f3c4ce55af1cec3666648de
Nitik Mishra <nitikmishra29@gmail.com>
2026-08-01 12:13:08 +0530

    Time: 0 ms (100%), Space: 19.2 MB (79.49%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -4,8 +4,6 @@ class Solution:
         dp=list(nums)
         for i in range(n-2,-1,-1):
             for j in range(i+1,n):
-                if i==j:
-                    return True 
                 dp[j] = max(nums[i] - dp[j], nums[j] - dp[j - 1])
         return dp[-1] >= 0
 
