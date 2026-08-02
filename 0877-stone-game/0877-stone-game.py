84e9c6aa125035c0ed3bd0ce91beb30e899bdf77
Nitik Mishra <nitikmishra29@gmail.com>
2026-08-02 17:04:54 +0530

    Time: 0 ms (100%), Space: 19.3 MB (75.06%) - LeetHub

--- 0877-stone-game/0877-stone-game.py
+++ 0877-stone-game/0877-stone-game.py
@@ -1,5 +1,26 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
+        return True 
+        
+        #two pointer 
+        a=0
+        b=0
+        left=0
+        right = len(piles)-1
+        while left<=right:
+            if piles[left]>piles[right]:
+                chosen=piles[left]
+                left+=1
+            else:
+                 chosen=piles[right]
+                 right-=1
+            if True:
+                a+=chosen
+            else:
+                b+=chosen 
+        return a>b 
+
+       #second method (not good may caused index error)
         a=0
         b=0
         for i, pile in enumerate(piles):
