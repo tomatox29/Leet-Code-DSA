17912016f85e5bb6a7a555bb756cd92487e26e7c
Nitik Mishra <nitikmishra29@gmail.com>
2026-08-04 23:19:28 +0530

    Time: 0 ms (100%), Space: 19.4 MB (18.85%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.py
+++ 3731-find-missing-elements/3731-find-missing-elements.py
@@ -5,6 +5,17 @@ class Solution:
         set_nums=set(nums)
         return [x for x in range(start, end + 1) if x not in set_nums]
 
+        #second solution same idea differnt style 
+        nums.sort()
+        ans=[]
+        j=0
+        for i in range(nums[0],nums[-1]):
+            if nums[j]!=i:
+                a.append(i)
+            else:
+                j+=1
+        return ans
+
 
 
         
