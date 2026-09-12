fd233857370c7db5ca446b5a5b19ca33d85aaacb
Nitik Mishra <nitikmishra29@gmail.com>
2026-09-12 22:32:41 +0530

    Time: 1438 ms (79.66%), Space: 82.9 MB (42.37%) - LeetHub

---
+++ 3414-maximum-score-of-non-overlapping-intervals/3414-maximum-score-of-non-overlapping-intervals.py
@@ -0,0 +1,36 @@
+class Solution:
+    def maximumWeight(self, intervals: List[List[int]]) -> List[int]:
+        #i am unable to solve this my own, long way to go
+
+        n = len(intervals)
+        arr = [
+            (intervals[i][1], intervals[i][0], intervals[i][2], i)
+            for i in range(n)
+        ]
+        arr.sort(key=lambda x: x[0])
+
+        dp = [[0] * 5 for _ in range(n + 1)]
+        indices = [[[] for _ in range(5)] for _ in range(n + 1)]
+
+        for i in range(n):
+            r, l, weight, idx = arr[i]
+           
+            k = bisect_left(arr, (l,), hi=i)
+
+            for j in range(1, 5):
+                s1 = dp[i][j]
+                s2 = dp[k][j - 1] + weight
+                if s1 > s2:
+                    dp[i + 1][j] = dp[i][j]
+                    indices[i + 1][j] = indices[i][j].copy()
+                    continue
+
+                new_index = indices[k][j - 1].copy()
+                new_index.append(idx)
+                new_index.sort()
+                if s1 == s2 and indices[i][j] < new_index:
+                    new_index = indices[i][j].copy()
+                dp[i + 1][j] = s2
+                indices[i + 1][j] = new_index
+
+        return indices[n][4]
